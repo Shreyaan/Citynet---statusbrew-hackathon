6c739cd176c0dbbbb2fa2a2d633fb37645237e75
s <11375404+Shreyaan@users.noreply.github.com>
2024-09-07 19:22:57 +0530

    backend 0.01 (#1)
    * flask init
    
    * blueprint for event feature init
    
    * updated requirements.txt
    
    * venv init
    
    * jwt setup
    
    * Update requirements.txt
    
    * make model 1:1 to supabase
    
    * event_management0.001
    
    * setup r2 + fix db+ fix events
    
    * fix events, models , jwt
    
    * wsgi error
    
    * .
    
    * fix db models
    
    * feat + bugs
    
    * remove pyc files
    
    * ..
    
    * add approval flow
    
    * add tags for user
    
    * Update models.py
    
    * Update models.py
    
    * add events endpoint + models update
    
    * twillo
    
    * Update models.py
    
    * Update jwt_bp.py
    
    * fire system complete
    
    ---------
    
    Co-authored-by: ashboi005 <125044617+ashboi005@users.noreply.github.com>

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,32 @@
-from flask import Flask, request, session, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
-load_dotenv()
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Update CORS configuration
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+SECRET_KEY_SUPABASE = os.getenv("SECRET_KEY")
+
+# Create the SQLAlchemy engine
+engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+
+# Create a sessionmaker
+Session = sessionmaker(bind=engine)
+
+# Create a base class for declarative models
+Base = declarative_base()
+
 db = SQLAlchemy(app)
-CORS(app)
+
+
+# Function to get a database session
+def get_db_session():
+    return Session()
